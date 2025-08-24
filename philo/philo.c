0fa941583115b751c5bdd91a6b396ea6975e5711
Zainab Salih <zsalih@student.42abudhabi.ae>
2025-08-24 21:42:21 +0400

    Philo: project structure √

--- philo/philo.c
+++ philo/philo.c
@@ -3,35 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zsalih < zsalih@student.42abudhabi.ae>     +#+  +:+       +#+        */
+/*   By: zsalih <zsalih@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 15:13:36 by zsalih            #+#    #+#             */
-/*   Updated: 2025/08/13 00:21:29 by zsalih           ###   ########.fr       */
+/*   Updated: 2025/08/24 20:36:08 by zsalih           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    t_config config;
-    t_data   data;
-	pthread_t monitor;
+	t_config	config;
+	t_data		data;
+	pthread_t	monitor;
+	int			i;
 
-    if (!parse_args(ac, av, &config))
-        return (1);
-    if (!init_data(&data, &config))
-        return (1);
-    init_philos(&data);
-    for (int i = 0; i < data.config.nbr_philos; i++) {
-        if (pthread_create(&data.philos[i].thread_id, NULL, philo_routine, &data.philos[i]) != 0)
-            perror("pthread_create failed"); 
-    }
+	if (!parse_args(ac, av, &config))
+		return (1);
+	if (!init_data(&data, &config))
+		return (1);
+	init_philos(&data);
+	i = 0;
+	while (i < data.config.nbr_philos)
+	{
+		if (pthread_create(&data.philos[i].thread_id, NULL, philo_routine,
+				&data.philos[i]) != 0)
+			perror("pthread_create failed");
+		i++;
+	}
 	pthread_create(&monitor, NULL, monitor_routine, &data);
-    for (int i = 0; i < data.config.nbr_philos; i++) {
-        pthread_join(data.philos[i].thread_id, NULL); }
-    pthread_join(monitor, NULL);
-    // cleanup_data(&data);
-    return (0);
+	i = 0;
+	while (i < data.config.nbr_philos)
+	{
+		pthread_join(data.philos[i].thread_id, NULL);
+		i++;
+	}
+	pthread_join(monitor, NULL);
+	// cleanup_data(&data);
+	return (0);
 }
-
